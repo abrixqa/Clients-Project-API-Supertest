3cc65a29ebe77a917902795a6b5d4cd450c902a1
abrixqa <abrix.qa@gmail.com>
2023-04-19 21:38:56 -0500

    Add .env files

--- tests/auth.spec.js
+++ tests/auth.spec.js
@@ -1,12 +1,13 @@
 import { expect } from 'chai'
 import request from 'supertest'
+import 'dotenv/config'
 
 describe('Authorization tests', () => {
   describe('Authorization with valid data', async () => {
     it('Response status code is 200', async () => {
-      let res = await request('https://clientbase-server.herokuapp.com')
+      let res = await request(process.env.BASE_URL)
         .post('/v5/user/login')
-        .send({ email: 'sol@gmail.com', password: 'kEftok-jercaf-nyvny2' })
+        .send({ email: process.env.EMAIL, password: process.env.PASSWORD })
       console.log(res.body)
       expect(res.statusCode).to.eq(200)
     })
