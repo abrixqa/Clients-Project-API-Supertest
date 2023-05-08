9780a58ec08c445b12a99b0d63b647af425e8c44
abrixqa <abrix.qa@gmail.com>
2023-05-08 14:27:31 -0500

    get users tests

--- tests/register.spec.js
+++ tests/register.spec.js
@@ -1,5 +1,6 @@
 import { expect } from 'chai'
-import { reg, randomEmail } from '../helpers/general'
+import { reg, randomEmail, login } from '../helpers/general'
+import request from 'supertest'
 
 const chance = require('chance').Chance()
 describe('Registration', () => {
@@ -118,6 +119,40 @@ describe('Registration', () => {
     })
     it('Response body returns an error message', () => {
       expect(res.body.message).to.eq('User with this e-mail exists')
+    })
+  })
+})
+
+describe('Get registered users', () => {
+  describe('Get user by id', () => {
+    let response
+    let getUser
+    let id
+    before(async () => {
+      response = await login(process.env.EMAIL, process.env.PASSWORD)
+      id = response.body.payload.user._id
+      getUser = await request(process.env.BASE_URL)
+        .get(`/v5/user/${id}`)
+        .set('Authorization', process.env.TOKEN)
+      console.log(id)
+    })
+    it('Response body returns status code 200', () => {
+      expect(getUser.statusCode).eq(200)
+    })
+    it('Response body returns correct message', () => {
+      expect(getUser.body.message).eq('User found')
+    })
+  })
+  describe('Get all users', () => {
+    let response
+    before(async () => {
+      response = await request(process.env.BASE_URL)
+        .post('/v5/user/search')
+        .send({ limit: 100, page: 1 })
+        .set('Authorization', process.env.TOKEN)
+    })
+    it('Response body returns status code 200', () => {
+      expect(response.statusCode).eq(200)
     })
   })
 })
