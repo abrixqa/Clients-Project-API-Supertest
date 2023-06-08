6447bf05c9ce8c28bebd9415eae6df1ec3aa3f1a
abrixqa <abrix.qa@gmail.com>
2023-06-08 16:42:17 -0500

    added mochawesome

--- .mocharc.js
+++ .mocharc.js
@@ -5,3 +5,11 @@ module.exports = {
   ignore: 'tests/exampleTest.spec.js',
   file: 'global-hooks/config.js',
 }
+/*reporter: 'mochawesome',
+    reporterOptions: [
+    'reportDir=MyReports',
+    'reportFilename=updatedReport',
+    'json=false',
+  ],*/
+
+// 'reportFilename=[status]_[datetime]-updatedReport'
--- tests/register.spec.js
+++ tests/register.spec.js
@@ -1,5 +1,5 @@
 import { expect } from 'chai'
-import { reg, randomEmail, login } from '../helpers/general-helper'
+import { reg, randomEmail, login } from '../helpers/index'
 import request from 'supertest'
 
 const chance = require('chance').Chance()
